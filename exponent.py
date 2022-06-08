aa734d8a2fc69411cfcfbd2fa1c6b9118fff9a67
Christopher Turcotte <52133203+PyGuyDev@users.noreply.github.com>
2022-06-08 09:59:58 -0400

    Add files via upload

---
+++ exponent.py
@@ -0,0 +1,4 @@
+# exponent.py --> A program that takes two numbers from the user and uses those two numbers as a base and a power and gives the result.  # noqa: E501
+num1 = input('Enter a base number: ')
+num2 = input('Enter an exponent: ')
+print(f'{num1} to the power of {num2} = {int(num1) ** int(num2)}')
